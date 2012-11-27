8d4acb331308b23ec2f0739692fabaaf29432a38
Pierre Andrews <pierre.andrews@gmail.com>
2012-11-27 19:19:06 -0200

    move enviar and convidar to the angularjs templates

---
+++ frontend/app/scripts/controllers/selecionar_convidar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+frontendApp.controller('SelecionarConvidarCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
