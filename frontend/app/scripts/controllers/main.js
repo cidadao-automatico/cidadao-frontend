348b4f78e9ad38cff2b47c9c50c7de8d638ac7f2
Pierre Andrews <pierre.andrews@gmail.com>
2012-11-27 19:00:03 -0200

    templating

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-cidadoAutomaticoApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+cidadoAutomaticoApp.controller('MainCtrl', function($scope, cidadolei) {
+  $scope.laws = cidadolei.getLaws();
 });
