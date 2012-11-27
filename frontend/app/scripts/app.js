398e5f7e77d08c24e2287e250d1ab660ea7d16a6
Pierre Andrews <pierre.andrews@gmail.com>
2012-11-27 16:25:24 -0200

    change structure to use yeoman angular seed, fix css to match

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var cidadoAutomaticoApp = angular.module('cidadoAutomaticoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+cidadoAutomaticoApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
