be4863ed213bac1b72a9d0dbb5f1734c0933dee2
tavoaqp <tavoaqp@gmail.com>
2013-09-24 21:57:49 -0300

    Deploy using Capistrano

---
+++ frontend-0.2/config/deploy/production.rb
@@ -0,0 +1,4 @@
+set :repository,  "git@github.com:cidadao-automatico/cidadao-frontend.git"
+set :branch, 'production'
+set :application, "vigiapolitico-frontend-production"
+set :deploy_to, "/home/#{user}/apps/#{application}"
---
+++ frontend-0.2/config/deploy/staging.rb
@@ -0,0 +1,4 @@
+set :repository,  "git@github.com:cidadao-automatico/cidadao-frontend.git"
+set :branch, 'feature/staging'
+set :application, "vigiapolitico-frontend-staging"
+set :deploy_to, "/home/#{user}/apps/#{application}"
