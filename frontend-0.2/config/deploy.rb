477c6699de8d2ccabef78b795a5a7c3db3913f83
tavoaqp <tavoaqp@gmail.com>
2013-09-26 00:55:50 -0300

    Fixing deploy to run grunt build

--- frontend-0.2/config/deploy.rb
+++ frontend-0.2/config/deploy.rb
@@ -1,8 +1,10 @@
 require 'capistrano/ext/multistage'
 require 'capistrano/confirm'
+require "rvm/capistrano"                               # Load RVM's capistrano plugin.
 
 set :stages, %w(production staging)
 set :default_stage, "staging"
+set :current_frontend, "frontend-0.2"
 
 set :user, 'ubuntu'
 set :scm, :git
@@ -10,8 +12,11 @@ set :scm_verbose, false
 set :keep_releases, 5
 
 set :user, 'ubuntu'
-set :use_sudo, false
+set :use_sudo, true
 set :confirm_stages, "production"
+
+set :rvm_type, :system
+set :rvm_bin_path, '/usr/local/rvm/bin'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -22,8 +27,31 @@ role :db,  "julgamento.clipesebandas.com.br", :primary => true # This is where R
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
+after "deploy:cleanup", "npm:install"
+after "npm:install", "bower:install"
+after "bower:install", "grunt:build"
 
+namespace :npm do
+  desc 'Install npm packages'
+  task :install, :roles => :web do
+    run "cd #{deploy_to}/current/#{current_frontend} && rvmsudo npm install"
+  end
+end
 
+namespace :bower do
+  desc 'Install bower components'
+  task :install, :roles => :web do
+    run "cd #{deploy_to}/current/#{current_frontend} && bower install"
+  end
+end
+
+namespace :grunt do
+  desc 'Build javascript code using grunt'
+  task :build, :roles => :web do
+    run "cd #{deploy_to}/current/#{current_frontend} && grunt build"
+  end
+end
+#grunt grunt-cli connect-livereload load-grunt-tasks
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
