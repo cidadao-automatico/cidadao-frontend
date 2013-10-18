aab05ac29f8593be43cd6af453dc617a4ddcf6c5
tavoaqp <tavoaqp@gmail.com>
2013-10-18 17:14:54 -0300

    Trying to hide/show law description

--- frontend-0.2/app/scripts/controllers/laws_dashboard.js
+++ frontend-0.2/app/scripts/controllers/laws_dashboard.js
@@ -7,4 +7,42 @@ angular.module('vigiaPoliticoApp')
   		console.log($scope.laws)
   	})
     
+    $scope.activeLawIndex=0;
+    $scope.showExtended = false;
+
+    $scope.showNormalLaw = function(index){
+      var result=$scope.activeLawIndex==index && $scope.showExtended == false;
+      console.log("showNormalLaw: " + result)
+      return result;
+    }
+
+    $scope.showExtendedLaw = function(index){
+      var result=$scope.activeLawIndex==index && $scope.showExtended == true;
+      console.log("showExtendedLaw: " + result)
+      return result;
+    }
+
+    $scope.enableExtendedLaw = function(index){
+      $scope.activeLawIndex=index;
+      $scope.showExtended = true;      
+    }
+
+    $scope.disableExtendedLaw = function(index){
+      $scope.activeLawIndex=index;
+      $scope.showExtended = false;      
+    }    
+
+    $scope.showLawDescription = function (index) {    	
+        console.log("aaaaaaaaaaaaaa")
+        $scope.activeLawIndex = index;
+        console.log("active index "+$scope.activeLawIndex)
+    };
+    
+    $scope.isShowing = function(index){
+    	
+    	var result=($scope.activeLawIndex == index)
+    	console.log("is showing activeLawindex: "+$scope.activeLawIndex+" index:"+index)
+        return  result;
+    };
+
   });
