b25c22356ebf25b10a65eca6c1dff8a5a0e35599
tavoaqp <tavoaqp@gmail.com>
2013-10-18 17:53:02 -0300

    Initial effect to show law description

--- frontend-0.2/app/scripts/controllers/laws_dashboard.js
+++ frontend-0.2/app/scripts/controllers/laws_dashboard.js
@@ -7,42 +7,21 @@ angular.module('vigiaPoliticoApp')
   		console.log($scope.laws)
   	})
     
-    $scope.activeLawIndex=0;
-    $scope.showExtended = false;
-
-    $scope.showNormalLaw = function(index){
-      var result=$scope.activeLawIndex==index && $scope.showExtended == false;
-      console.log("showNormalLaw: " + result)
-      return result;
+    //#FIXME: Seria legal utilizar Angular ngshow e nghide
+    $scope.showExtendedLaw = function(lawId)
+    {
+      var normal=$("#law_container_"+lawId+"_normal")
+      var extended=$("#law_container_"+lawId+"_extended")
+      normal.hide("slow")
+      extended.show("slow")
     }
 
-    $scope.showExtendedLaw = function(index){
-      var result=$scope.activeLawIndex==index && $scope.showExtended == true;
-      console.log("showExtendedLaw: " + result)
-      return result;
+    $scope.hideExtendedLaw = function(lawId)
+    {
+      var normal=$("#law_container_"+lawId+"_normal")
+      var extended=$("#law_container_"+lawId+"_extended")
+      normal.show("slow")
+      extended.hide("slow")
     }
-
-    $scope.enableExtendedLaw = function(index){
-      $scope.activeLawIndex=index;
-      $scope.showExtended = true;      
-    }
-
-    $scope.disableExtendedLaw = function(index){
-      $scope.activeLawIndex=index;
-      $scope.showExtended = false;      
-    }    
-
-    $scope.showLawDescription = function (index) {    	
-        console.log("aaaaaaaaaaaaaa")
-        $scope.activeLawIndex = index;
-        console.log("active index "+$scope.activeLawIndex)
-    };
-    
-    $scope.isShowing = function(index){
-    	
-    	var result=($scope.activeLawIndex == index)
-    	console.log("is showing activeLawindex: "+$scope.activeLawIndex+" index:"+index)
-        return  result;
-    };
 
   });
