5c67720c5bfa5c8cd40a63246aa6ca2d74a99034
tavoaqp <tavoaqp@gmail.com>
2013-11-09 15:04:46 -0200

    Corrigindo branch de deploy e div de lei estendida

--- frontend-0.2/config/deploy/production.rb
+++ frontend-0.2/config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :repository,  "git@github.com:cidadao-automatico/cidadao-frontend.git"
-set :branch, 'production'
+set :branch, 'teste1'
 set :application, "vigiapolitico-frontend-production"
 set :deploy_to, "/home/#{user}/apps/#{application}"
